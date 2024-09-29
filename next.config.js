4206449c35152b1bd39387dc84e9fe4f10cb9fdf
Sushant Daga <dsush2307@gmail.com>
2024-09-30 00:11:15 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 // next.config.js
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = '/website/out/'
-let basePath = '/website/out'
+let assetPrefix = '/website/'
+let basePath = '/website'
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
