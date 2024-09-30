44eeb9055f3c8af36bda2680df28125ef7171d9a
SushantDaga <SushantDaga@users.noreply.github.com>
2024-09-30 19:28:17 +0000

    deploy: ab0d7d2b4aeff8ca754017bacfa978b6f8ad3430

--- _next/static/lgUnAUkq1yfiFZqSTeXNh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/73738821fd16f859.css","static/chunks/pages/index-5180b006347850d4.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/qd4Ly-pmT5J05TNmYh23w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/73738821fd16f859.css","static/chunks/pages/index-16cd80bbe40430a1.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
