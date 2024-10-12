d4a0dbb810cc946d9fbbcbdb3451d928f94e72b4
SushantDaga <SushantDaga@users.noreply.github.com>
2024-10-12 10:38:15 +0000

    deploy: d27a7cbb2a25e969859f680278b0729cf7f6f53f

--- _next/static/pjCy8QpN6iSsZVtiTiwG3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/73738821fd16f859.css","static/chunks/pages/index-733f0b7f0b5fb526.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
